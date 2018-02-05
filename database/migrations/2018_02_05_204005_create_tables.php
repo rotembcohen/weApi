a98ce4033ae7eb9ccac31ca195a4635796cca677
Rotem Cohen <rotem@timeflash.com>
2018-02-05 15:53:18 -0500

    initial table migrations

---
+++ database/migrations/2018_02_05_204005_create_tables.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('iso2');
+            $table->string('shortName');
+            $table->string('longName');
+            $table->string('iso3');
+            $table->string('numCode');
+            $table->timestamps();
+        });
+        Schema::create('markets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('market');
+            $table->timestamps();
+        });
+        Schema::create('properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('desks');
+            $table->integer('Sf');
+            $table->text('address1');
+            $table->text('address2');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postalCode');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->foreign('countryId')
+                ->references('id')
+                ->on('countries')
+                ->onDelete('cascade');
+            $table->integer('regionalCategory');
+            $table->foreign('marketId')
+                ->references('id')
+                ->on('markets')
+                ->onDelete('cascade');
+            $table->integer('submarketId');
+            $table->integer('locationId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+        Schema::dropIfExists('markets');
+        Schema::dropIfExists('countries');
+    }
+}
