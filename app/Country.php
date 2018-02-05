ab95658f12fd56e958b9d2c576cf574198f68339
Rotem Cohen <rotem@timeflash.com>
2018-02-05 18:13:43 -0500

    model files

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    protected $fillable = [
+        "iso2",
+        "shortName",
+        "longName",
+        "iso3",
+        "numCode",
+    ];
 }
--- app/Property.php
+++ app/Property.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'desks',
+        'Sf',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'postalCode',
+        'latitude',
+        'longitude',
+        'regionalCategory',
+        'submarketId',
+        'locationId',
+    ];
 }
