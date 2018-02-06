0708bd143a0ce88e7ee69a86a9fcee8831f05b84
Rotem Cohen <rotem@timeflash.com>
2018-02-06 11:27:38 -0500

    unit tests init

---
+++ database/seeds/TestSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Market::class, 5)->create();
+        factory(App\Country::class, 5)->create();
+        // factory(App\Property::class, 3)->create();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,26 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Artisan;
+use DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+        DB::beginTransaction();
+        Artisan::call('migrate:refresh');
+        $this->seed('TestSeeder');
+    }
+
+
+    protected function tearDown()
+    {
+        Artisan::call('migrate:reset');
+        DB::rollBack();
+        parent::tearDown();
+    }
 }
